bb59af91b510af631e3229d06b2948ea6a0e5687
Ivaylo Atanasov <ivailo.atanasov93@gmail.com>
2022-03-03 18:05:20 +0000

    Add USE TLS setting

--- website/settings.py
+++ website/settings.py
@@ -163,6 +163,7 @@ STATICFILES_DIRS = (
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_SSL = True
+EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'dimitar.kanchev.website@gmail.com'
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')  # Environmental variable
 EMAIL_PORT = 465
