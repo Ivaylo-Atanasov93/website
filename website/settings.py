302a67037a02318fc1f98162d29e5edfe2246c8b
Ivaylo Atanasov <ivailo.atanasov93@gmail.com>
2022-03-03 16:40:29 +0000

    Add COLLECTSTATIC variable

--- website/settings.py
+++ website/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = '***********************************************'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
+DISABLE_COLLECTSTATIC = False
 ALLOWED_HOSTS = ['herokuapp.com', '127.0.0.1.']
 
 LOGGING = {
