e05802620f9b84a8a6053c90b40a08262b0779af
Ivaylo Atanasov <ivailo.atanasov93@gmail.com>
2022-03-03 17:03:20 +0000

    Edit allowed hosts

--- website/settings.py
+++ website/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = '***********************************************'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 DISABLE_COLLECTSTATIC = False
-ALLOWED_HOSTS = ['herokuapp.com', '127.0.0.1.']
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1.']
 
 LOGGING = {
     'version': 1,
